832f872eb5b6c165199a22f04466db5e959780ee
aniketchavan2211 <aniketchavan2211@gmail.com>
2024-03-05 20:44:08 +0530

    Modified args ans kwargs in timer.py

--- module/timer.py
+++ module/timer.py
@@ -5,7 +5,7 @@ import time
 def timer(func):
     def wrapper(*args, **kwargs):
         start = time.time()
-        rv = func()
+        rv = func(*args, **kwargs)
         total = time.time() - start
         hours, remainder = divmod(total, 3600)
         minutes, seconds = divmod(remainder, 60)
@@ -13,7 +13,7 @@ def timer(func):
         microseconds = int((seconds % 1) * 1e6)
         nanoseconds = int((seconds % 1) * 1e9)
 
-        print(f"Time: {int(hours)} hrs {int(minutes)} min {int(seconds)} sec {milliseconds} ms {microseconds} µs {nanoseconds} ns")
+        print(f"Time Taken: {int(hours)} hrs {int(minutes)} min {int(seconds)} sec {milliseconds} ms {microseconds} µs {nanoseconds} ns")
         return rv
     return wrapper
 
